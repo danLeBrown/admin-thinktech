2e9cc11ddffeb9acaa94f753555b89f7d716bae6
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-05-27 14:09:58 +0100

    updated nuxt

--- plugins/asset.js
+++ plugins/asset.js
@@ -1,5 +1,5 @@
 export default function (context, inject) {
   inject('asset', (path) => {
-    return `http://localhost:8000/storage/${path}`
+    return `https://thinktech.fuoye360.com/storage/${path}`
   })
 }
